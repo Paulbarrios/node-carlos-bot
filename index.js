e66f00dc91ed08950587fb892900a147f29cbdb2
Carlos <kace91@gmail.com>
2018-03-08 22:32:17 +0100

    subiendo lo que habia

---
+++ index.js
@@ -0,0 +1,27 @@
+const Telegraf = require ('telegraf');
+
+const bot = new Telegraf('516551398:AAExf-IPWm3em7Itmi14mFWSD8JJgMtRBIA');
+bot.start((ctx) => {
+  console.log('started:', ctx.from.id)
+  return ctx.reply('Hola, soy de instagram. te escribo por si quieres que te demos dinero gratis.')
+
+})
+
+
+bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
+
+
+bot.command('help', (ctx) => ctx.reply('Try send a sticker!'))
+bot.hears('hi', (ctx) => ctx.reply('Hey there!'))
+bot.hears(/buy/i, (ctx) => ctx.reply('Buy-buy!'))
+bot.on('sticker', (ctx) => ctx.reply('👍'))
+bot.on('text', (ctx) => {
+  while(true){
+    ctx.reply('👍');
+  }
+  return null;
+}
+)
+console.log('hasta aqui bien')
+
+bot.startPolling()
